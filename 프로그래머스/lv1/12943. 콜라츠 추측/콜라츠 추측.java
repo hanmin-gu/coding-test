a55d1915fd519de98d893e10b64dcc379fe60ce8
한민구 <115986121+hanmin-gu@users.noreply.github.com>
2023-03-01 07:03:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long number = (long)num;
+        while(number != 1){
+            
+            if(number % 2 != 0){
+                number = (number * 3) + 1;
+                answer++;
+            }
+             if(number % 2 == 0){
+                number = number / 2;
+                answer++;
+            }
+            if(answer == 500){ 
+                answer = -1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
